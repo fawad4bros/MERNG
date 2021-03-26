4d764c7a84ee8419759b2b4f50a506e3eaebf28e
fawad4bros <fawad4bros@gmail.com>
2021-03-26 21:56:46 +0500

    .

---
+++ index.js
@@ -0,0 +1,13 @@
+const { ApolloServer } = require('apollo-server');
+const gql = require('graphql-tag');
+
+const typeDefs = gql`
+    type Query{
+        sayHi: String!
+    }
+`
+const resolvers = {
+    Query:{
+        
+    }
+}
